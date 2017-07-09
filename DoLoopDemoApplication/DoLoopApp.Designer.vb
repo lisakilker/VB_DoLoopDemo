7594fcae286e79fc1313d07325829f3f77eed626
Lisa Kilker <lisakilker@users.noreply.github.com>
2017-07-09 19:08:05 +0200

    Final
    Final

---
+++ DoLoopDemoApplication/DoLoopApp.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDoLoopApp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(39, 319)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 0
+        Me.btnRun.Text = "Run!"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(171, 319)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'frmDoLoopApp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 361)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRun)
+        Me.Name = "frmDoLoopApp"
+        Me.Text = "Do Loop Demo"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnRun As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ DoLoopDemoApplication/DoLoopApp.vb
@@ -0,0 +1,118 @@
+﻿Public Class frmDoLoopApp
+
+    Private Sub frmDoLoopApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Init()
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        'Print("Hi!")
+        'Print(42)
+        'Print(19.74)
+        'Dim input As String = InputBox("Enter a value to display", "Input required")
+        'Print(input)
+        '----------------------
+        'Const cintDataPieces As Integer = 10
+        'Dim intCounter As Integer = 0
+        'Dim strInput As String
+
+        'Do While intCounter < cintDataPieces
+        'strInput = InputBox("Enter something")
+        'Print(strInput)
+        'intCounter += 1
+        'Loop
+        '-------------------------
+
+        ' Const cintDataPieces As Integer = 10
+        ' Dim intCounter As Integer = 0
+        ' Dim strInput As String
+        ' Do
+        'strInput = InputBox("Enter something.")
+        ' Print(strInput)
+        'intCounter = intCounter + 1
+        'Loop Until strInput = ""
+        '----------------------------
+        'Const cintDataPieces As Integer = 7
+        'Dim intCounter As Integer = 0
+        'Dim strInput As String
+
+        'Do While intCounter < cintDataPieces
+        'strInput = InputBox("Enter Something")
+        'If IsNumeric(strInput) Then
+        'Print(strInput)
+        'intCounter = intCounter + 1
+        'Else
+        'MsgBox("Numeric input Only")
+        'End If
+        'Loop
+        '----------------------------------------
+        'Const cintDataPieces As Integer = 12
+        'Dim intCounter As Integer = 0
+        'Dim strInput As String
+        ' Do
+        'strInput = InputBox("Enter something.")
+        'Print(strInput)
+        'intCounter = intCounter + 1
+        'Loop Until intCounter >= cintDataPieces Or strInput = ""
+        '-----------------------------------------
+        Const cintDataPieces As Integer = 11
+        Dim intCounter As Integer = 0
+        Dim intTotal As Integer = 0
+        Dim strInput As String
+        Do
+            strInput = InputBox("Enter something.")
+            If IsNumeric(strInput) Then
+                Print(strInput)
+                intCounter = intCounter + 1
+                intTotal = intTotal + Convert.ToInt32(strInput)
+            Else
+                MsgBox("Please enter a valid number")
+            End If
+        Loop Until intCounter >= cintDataPieces Or strInput = ""
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Clear()
+    End Sub
+    ' Magic code :-)
+    ' Don't change anything below
+
+    Private lstDisplay As ListBox
+    Private lblCount As Label
+
+    Private Sub Clear()
+        lstDisplay.Items.Clear()
+        UpdateCount()
+    End Sub
+    Private Sub Init()
+        lstDisplay = New ListBox()
+        lstDisplay.Location = New Point(15, 15)
+        lstDisplay.Size = New Size(250, 200)
+        Me.Controls.Add(lstDisplay)
+
+        lblCount = New Label()
+        lblCount.Location = New Point(15, 215)
+        UpdateCount()
+        Me.Controls.Add(lblCount)
+    End Sub
+
+    Private Sub Print(value As Integer)
+        Print(value.ToString("N0"))
+    End Sub
+
+    Private Sub Print(value As Double)
+        Print(value.ToString("N"))
+    End Sub
+
+    Private Sub Print(value As String)
+        lstDisplay.Items.Add(value)
+        UpdateCount()
+    End Sub
+
+    Private Sub UpdateCount()
+        lblCount.Text = "Count: " & lstDisplay.Items.Count.ToString()
+    End Sub
+
+    ' End of magic code
+
+End Class
